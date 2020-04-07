763ef3c0598edc3ce6d68b0b5bcd96013786d022
Rajendra Singh Bhati <bhati.rajendra007@hotmail.com>
2020-04-08 03:44:28 +0530

    Adding release 1 for 1st test

---
+++ CoreWeb/Controllers/CmsController.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Copyright (c) 2017 Håkan Edling
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ * 
+ * http://github.com/tidyui/coreweb
+ * 
+ */
+
+using Microsoft.AspNetCore.Mvc;
+using Piranha;
+using System;
+
+namespace CoreWeb.Controllers
+{
+    /// <summary>
+    /// Simple controller for handling the CMS content from Piranha.
+    /// </summary>
+    public class CmsController : Controller
+    {
+        /// <summary>
+        /// The private api.
+        /// </summary>
+        private readonly IApi api;
+
+        /// <summary>
+        /// Default construtor.
+        /// </summary>
+        /// <param name="api">The current api</param>
+        public CmsController(IApi api) {
+            this.api = api;
+        }
+
+        /// <summary>
+        /// Gets the page with the specified id.
+        /// </summary>
+        /// <param name="id">The unique id</param>
+        [Route("page")]
+        public IActionResult Page(Guid id) {
+            var model = api.Pages.GetById<Models.StandardPage>(id);
+            ViewBag.CurrentPage = model.Id;
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// Gets the post with the specified id.
+        /// </summary>
+        /// <param name="id">The unique id</param>
+        [Route("post")]
+        public IActionResult Post(Guid id) {
+            var model = api.Posts.GetById<Models.BlogPost>(id);
+            ViewBag.CurrentPage = model.BlogId;
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// Gets the page with the specified id.
+        /// </summary>
+        /// <param name="id">The unique id</param>
+        /// <param name="startpage">If this is the site startpage</param>
+        [Route("teaserpage")]
+        public IActionResult TeaserPage(Guid id, bool startpage) {
+            var model = api.Pages.GetById<Models.TeaserPage>(id);
+            ViewBag.CurrentPage = model.Id;
+
+            if (startpage)
+                return View("Start", model);
+            return View(model);
+        }
+
+        /// <summary>
+        /// Gets the archive for the category with the specified id.
+        /// </summary>
+        /// <param name="id">The category id</param>
+        /// <param name="year">The optional year</param>
+        /// <param name="month">The optional month</param>
+        /// <param name="page">The optional page</param>
+        /// <param name="category">The optional category id</param>
+        [Route("archive")]
+        public IActionResult Archive(Guid id, int? year = null, int? month = null, int? page = null, Guid? category = null) {
+            var model = api.Archives.GetById<Models.BlogPage>(id, page, category, year, month);
+            ViewBag.CurrentPage = model.Id;
+
+            return View(model);
+        }
+    }
+}
